393f399b45bd6a93aca4b88d5ef66ce7fb4fcf18
Shantanu Sharma <shantanu24sharma@gmail.com>
2026-06-04 17:05:15 -0400

    refactor: implement repeatString and reverseString functions with tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let finalString = ""
+    if (num>= 0){
+        for (let i=0;i<num;i++){
+            finalString = finalString + str
+        }
+        return finalString
+    }
+    else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let finalString = []
+    for (let i=str.length-1;i>=0;i--){
+      finalString.push(str[i])
+    }
+    return finalString.join("")
 };
 
 // Do not edit below this line
