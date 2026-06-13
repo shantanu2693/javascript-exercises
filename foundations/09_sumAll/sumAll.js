7e11e1785abfb52281dda572347be1a54d51342a
Shantanu Sharma <shantanu24sharma@gmail.com>
2026-06-13 09:41:03 -0400

    feat: implement removeFromArray function and corresponding tests
    - Added removeFromArray function to filter out specified values from an array.
    - Created package.json for the removeFromArray module with Jest as a dev dependency.
    - Updated removeFromArray.spec.js to include tests for single and multiple value removals, handling of non-present values, and type-specific removals.
    
    feat: enhance sumAll function with error handling and range summation
    
    - Modified sumAll function to validate inputs and return an error message for invalid cases.
    - Updated the implementation to calculate the sum of all integers within the specified range.
    - Revised sumAll.spec.js to include tests for various scenarios including large numbers and error cases.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (!(Number.isInteger(a) && Number.isInteger(b) && (a>0) && (b>0))){
+        return "ERROR"
+    }
+    else {
+        let sum = 0
+        start = Math.min(a,b)
+        end = Math.max(a,b)
 
+        for (let i =start;i<=end;i++){
+            sum += i
+        }
+        return sum
+    }
 };
 
 // Do not edit below this line
